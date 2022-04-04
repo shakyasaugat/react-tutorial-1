2a77619061e38dfefdc4f43440dbcffe892aa91a
Saugat Shakya <85560608+shakyasaugat@users.noreply.github.com>
2022-04-04 17:05:56 +0545

    [MODIFIED]: NExt page modified

--- react-attempt1/src/PostList.js
+++ react-attempt1/src/PostList.js
@@ -1,24 +1,48 @@
-import React, {useState, useEffect } from 'react';
+import React, {Component, useEffect } from 'react';
 import CardMediaExample from "./CardMediaExample";
 
-const PostList = props => {
-    const [post, getPost] = useState([]);
-    const API = 'http://dtmv.local/wp-json/wp/v2/posts';
-    const fetchPost = () => {
+export default class PostList extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            post: [],
+            offset: 0,
+            perPage: 10,
+            page: 1
+
+        };
+        this.handlePageClick = this.handlePageClick.bind(this);
+        
+    }
+    receivedData() {
+        const page = this.state.page;
+        const API = `http://dtmv.local/wp-json/wp/v2/posts/?page=${page}`;
+
         fetch(API)
         .then((res) => res.json())
         .then((res) => {
-               getPost(res)
+            // const post = Object.keys(res).map( (item, index) => <CardMediaExample key={index} attribute={item}/> );
+            const nextPage = this.state.page + 1;
+            this.setState({
+                post: res,
+                page: nextPage
+            });
         })
     }
-    useEffect(() => {
-        fetchPost()
-    }, [])
+    render() {
+        return (
+            <div className="post-list-wrapper">
+                {this.state.post.map( (item, index) => <CardMediaExample key={index} attribute={item}/> )}
+                <button onClick={this.handlePageClick}>Next</button>
+            </div>
+        )
+    }
+    handlePageClick = (e) => {
+        e.preventDefault();
+        this.receivedData();
 
-    return (
-        <div className="post-list-wrapper">
-            {post.map( (item, index) => <CardMediaExample key={index} attribute={item}/> )}
-        </div>
-    )
+    };
+    componentDidMount() {
+        this.receivedData()
+    }
 }
-export default PostList;
